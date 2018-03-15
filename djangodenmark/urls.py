187a0a5c3a801f470a68fccd7f87abd30a318cde
Benjamin Bach <benjamin@overtag.dk>
2018-03-15 20:41:18 +0100

    Add front page

--- djangodenmark/urls.py
+++ djangodenmark/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http.response import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda r: HttpResponse("Hello meow oink woof"))
 ]
