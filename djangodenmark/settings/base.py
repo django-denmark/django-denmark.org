57add51e174993f6489950a6e1c0233d80271e43
Benjamin Bach <benjamin@overtag.dk>
2018-03-16 00:09:18 +0100

    remove stuff only defined in dev or production

--- djangodenmark/settings/base.py
+++ djangodenmark/settings/base.py
@@ -15,14 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
