8312f1723161b46736387797d0395925a9285829
reaz uddin mahmood <reaz.u.mahmood@gmail.com>
2018-04-04 15:32:44 +0200

    matched maps.forms to original version

--- map/forms.py
+++ map/forms.py
@@ -1,5 +1,4 @@
 from django.contrib.gis import forms
-from mapwidgets.widgets import GooglePointFieldWidget, GoogleStaticOverlayMapWidget
 
 from . import models
 
@@ -9,6 +8,14 @@ class MapEntryForm(forms.ModelForm):
     class Meta:
         model = models.MapEntry
         widgets = {
-             'location': GooglePointFieldWidget,
-         }
+            'location': forms.OSMWidget(
+                attrs={
+                    'map_width': 800,
+                    'map_height': 500,
+                    'default_zoom': 7,
+                    'default_lat': 56.242,
+                    'default_lon': 11.634,
+                },
+            )
+        }
         fields = ('name', 'location')
