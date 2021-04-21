438e5834b951aa7e5a3220a9adc02df948ab1e42
Benjamin Bach <benjamin@overtag.dk>
2021-04-21 22:06:34 +0200

    Fix "os" not defined and use new pathlib

--- djangodenmark/settings/production.py
+++ djangodenmark/settings/production.py
@@ -3,12 +3,12 @@ This is the shared, PUBLIC production settings.
 
 Do NOT put secrets here.
 """
-
 from .base import *  # noqa
+from .base import BASE_DIR  # noqa
 
 
 DEBUG = False
 
 ALLOWED_HOSTS = ["django-denmark.org", "www.django-denmark.org"]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = str(BASE_DIR / "static")
