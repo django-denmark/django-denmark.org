c149fa42001cdbe1997454b76d57d270f8827ebc
Holmern <chri63dj@stud.kea.dk>
2021-05-04 12:45:11 +0200

    Halvejs med sign

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from .forms import InputForm
+   
+# Create your views here.
+def home_view(request):
+    context ={}
+    context['form']= InputForm()
+    return render(request, "signup.html", context)
 
 
 class SignUpView(generic.CreateView):
