420f5592c44d4f6210f3b9c2a0c80f48f4c8d5d7
Nadia Hansen <nadiahansen.kea@gmail.com>
2021-05-04 13:38:37 +0200

    overview begun

--- company/views.py
+++ company/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
 from company.models import CompanyForm, AddressForm, ContactInformationForm
 from django.views.generic.edit import FormView
+from company.models import companyForm, addressForm, contactInformationForm, company, address
+from django.views.generic import FormView, ListView
 
 # Create your views here.
-class CompanyView(TemplateView):
-    template_name = "company/companyoverview.html"
+# class CompanyView(TemplateView):
+#     template_name = "company/companyoverview.html"
 
 # class CompanyFormView(FormView):
 #     template_name = 'company/createCompanyProfile.html'
@@ -36,3 +38,10 @@ def companyFormView(request):
         contactInformationForm = ContactInformationForm()
 
     return render(request, 'company/createCompanyProfile.html', {'companyForm': companyForm, 'addressForm': addressForm, 'contactInformationForm': contactInformationForm})
+    def form_valid(self, form):
+        print("Hurra det virker!!!!!! jaaaaaa det gør det hurraaaa !!!!")
+        return super().form_valid(form)
+
+class CompanyView(ListView):
+    model = company
+    template_name = "company/companyoverview.html"
