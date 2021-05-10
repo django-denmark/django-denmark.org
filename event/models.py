f6004117cf75aa7a57ef48230145868cb5ce1d04
Mille737 <mille737@gmail.com>
2021-05-10 13:28:46 +0200

    events added with scroll

--- event/models.py
+++ event/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    eventTitle = models.CharField(max_length=100)
+    date = models.DateField()
+    location = models.CharField(max_length=145)
+    description = models.TextField()
+    eventUrl = models.CharField(max_length = 100)
+    eventImage = models.ImageField(upload_to = 'images/', null = True)
+  
+    
+    def __str__(self):
+        return '{} {} {} {} {}'.format(self.eventTitle, self.date, self.location, self.description, self.eventUrl)
